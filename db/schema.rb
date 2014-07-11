cbaf4b9867c63d190c00dd5a588576eeaaa8c9eb
Chris Dziewa <chris.dziewa@gmail.com>
2014-07-11 11:00:22 -0500

    Add user_id column to blog posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711154542) do
+ActiveRecord::Schema.define(version: 20140711155805) do
 
   create_table "blog_posts", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
