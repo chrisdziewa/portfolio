81b195fd30c0cdc4713797afe9dd393775abf89d
Chris Dziewa <chris.dziewa@gmail.com>
2014-07-11 12:32:01 -0500

    Add comment model and associations with blog post

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,5 +1,6 @@
 class BlogPost < ActiveRecord::Base
 	belongs_to :user
+	has_many :comments
 
 	validates :title, presence: { message: "Must include a title" }, 
 						length: { minimum: 5 }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711161358) do
+ActiveRecord::Schema.define(version: 20140711172816) do
 
   create_table "blog_posts", force: true do |t|
     t.string   "title"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20140711161358) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "author"
+  end
+
+  create_table "comments", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
